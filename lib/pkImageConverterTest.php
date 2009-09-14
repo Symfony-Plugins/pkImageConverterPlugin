3c639024fab15286c4d4d73cc6df134ce675d958
boutell <boutell@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-09-13 20:05:09 -0500

    Short, nonstandard, but occasionally useful test program for pkImageConverter which was used to validate the GD support
    git-svn-id: http://svn.symfony-project.com/plugins/pkImageConverterPlugin/trunk@21997 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/pkImageConverterTest.php
@@ -0,0 +1,9 @@
+<?php
+
+require 'pkImageConverter.class.php';
+
+// pkImageConverter::scaleToFit("testin.jpg", "testoutscaletofit.jpg", 400, 300);
+// pkImageConverter::scaleByFactor("testin.jpg", "testoutscalebyfactor.jpg", 0.5);
+pkImageConverter::scaleToFit("testin.jpg", "testoutscaleoriginalbobbi.jpg", 340, 451);
+// pkImageConverter::cropOriginal("testin.jpg", "testoutcroporiginaltall.jpg", 100, 300);
+// pkImageConverter::scaleToNarrowerAxis("testin.jpg", "testoutscaletonarroweraxis.jpg", 300, 200);
